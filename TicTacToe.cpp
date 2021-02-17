925cddb2b23313e6f74d7ab50e9f70ea862837d9
w460229957 <meditationzhang@gmail.com>
2021-02-17 12:06:00 +0800

    fixed initialization issue about Board(). Added display function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <iostream>
 
 using namespace std;
 
@@ -9,10 +10,46 @@ struct Board
 
 Board CreateBoard(){
     Board b1;
+    for (int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            /* code */
+            b1.arr_[i][j] = 0;
+        }
+        
+    }
     return b1;
+}
+
+void Display(Board board){
+    for (int i = 0; i < 3; i++)
+    {
+        /* code */
+        for (int j = 0; j < 3; j++)
+        {
+            /* code */
+            cout<<"||";
+            if (board.arr_[i][j] == 0)
+            {
+                /* code */
+                cout<<" ";
+            }
+            else if(board.arr_[i][j] == 1)
+            {
+                cout<<"X";
+            }
+            else if(board.arr_[i][j] == 2)
+            {
+                cout<<"O";
+            }
+        }
+        cout<<"||"<<endl;
+    }
     
 }
 
 int main(){
     Board b1 = CreateBoard();
+    Display(b1);
 }

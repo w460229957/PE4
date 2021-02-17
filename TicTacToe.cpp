a3484f468ca28955ae82a52ad01c9f78bdc8f4da
w460229957 <meditationzhang@gmail.com>
2021-02-17 11:36:37 +0800

    create Board()

---
+++ TicTacToe.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+using namespace std;
+
+struct Board
+{
+    int arr_[3][3];
+};
+
+Board CreateBoard(){
+    Board b1;
+    return b1;
+    
+}
+
+int main(){
+    Board b1 = CreateBoard();
+}
